7bd5f2ad911aa0403ae50515d5a30e2f5f17dd2f
Павел Руцкин <pavelru163@gmail.com>
2026-05-07 15:55:11 +0400

    NEW_Dajango10

--- config/urls.py
+++ config/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('catalog/', include('catalog.urls')),
+    path('', include('catalog.urls')),
     path('blog/', include('blog.urls')),
 ]
 if settings.DEBUG:
