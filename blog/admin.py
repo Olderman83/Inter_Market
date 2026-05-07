544e8fc06b9d2284a768ecaced19a8d8d60d6b8b
Павел Руцкин <pavelru163@gmail.com>
2026-05-07 12:08:11 +0400

    NEW_Dajango7

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import BlogPost
+
 
 # Register your models here.
+@admin.register(BlogPost)
+class BlogPostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'created_at', 'is_published', 'views_count']
+    list_display_links = ['title']
+    list_filter = ['is_published', 'created_at']
+    search_fields = ['title', 'content']
+    list_editable = ['is_published']
+    readonly_fields = ['views_count', 'created_at']
+    list_per_page = 20
+
+    fieldsets = (
+        ('Основная информация', {
+            'fields': ('title', 'content', 'preview')
+        }),
+        ('Публикация', {
+            'fields': ('is_published',)
+        }),
+        ('Статистика', {
+            'fields': ('views_count', 'created_at'),
+            'classes': ('collapse',)
+        }),
+    )
